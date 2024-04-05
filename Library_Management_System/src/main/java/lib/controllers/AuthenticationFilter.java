5f916786f71fc2ad56508801231c51cd69ffce9f
Mugisha Regis <mugisharegis72@gmail.com>
2024-04-05 18:13:43 +0200

    add authentication filter mapped to the /search url pattern

---
+++ Library_Management_System/src/main/java/lib/controllers/AuthenticationFilter.java
@@ -0,0 +1,73 @@
+package lib.controllers;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class AuthenticationFilter
+ */
+public class AuthenticationFilter extends HttpFilter implements Filter {
+
+	/**
+	 * @see HttpFilter#HttpFilter()
+	 */
+	public AuthenticationFilter() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+//		chain.doFilter(request, response);
+
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+		HttpSession session = httpRequest.getSession(false);
+
+		boolean isLoggedIn = (session != null && session.getAttribute("email") != null);
+		String loginURL = httpRequest.getContextPath() + "/login.jsp";
+
+		if (isLoggedIn) {
+			chain.doFilter(request, response);
+		} else {
+			httpResponse.sendRedirect(loginURL);
+		}
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	@Override
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
